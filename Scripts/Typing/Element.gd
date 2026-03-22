8024e4bb29f4e2c1d55f1e8862f6c643b6464f59
Jontaybr <Jontaybr@gmail.com>
2026-03-21 20:58:17 -0600

    Began overhualing the type system

---
+++ Scripts/Typing/Element.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name Element
+
+@export var name : String
+@export var strengths : Array[Element]
+@export var weakness : Array[Element]
+var scalar = {
+	"weak": 2.0,
+	"strong": 0.5
+}
