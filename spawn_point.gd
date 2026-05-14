39ab0757163850c2875268f5bee999c89f7bcfba
Jontaybr <Jontaybr@gmail.com>
2026-05-14 14:15:52 -0600

    spawnpoints respawn enemies on death

--- spawn_point.gd
+++ spawn_point.gd
@@ -6,19 +6,19 @@ extends Node2D
 
 var timer = 0
 
+func _ready() -> void:
+	spawn()
+
 func _process(delta: float) -> void:
 	if timer > 0:
 		timer -= delta
-	else:
-		timer = spawn_timer
-		if spawncount > 0:
-			spawn()
 
-func spawn(wait = false):
+func spawn(wait = true):
 	if wait:
 		await get_tree().create_timer(spawn_timer).timeout
 	var enemy = spawn_list.pick_random()
 	var new_spawn = enemy.scene.instantiate()
 	add_child(new_spawn)
 	new_spawn.global_position = global_position
+	new_spawn.died.connect(spawn)
 	spawncount -= 1
